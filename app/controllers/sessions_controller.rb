28ff0b5df8f2da13821bfef6d52c7af2a8244005
Vedika Birla <birla22v@mtholyoke.edu>
2015-03-22 18:15:51 -0400

    send back auth token everytime login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@
      self.resource = warden.authenticate(auth_options)
      if self.resource
        sign_in(resource_name, resource, store: false)
-       render json: {:user => current_user}, status: :ok
+       render json: {:user => current_user, :authentication_token => current_user.authentication_token}, status: :ok
      else
        render json: {:error => "Authentication Failure!"}, status: :unauthenticated
      end
