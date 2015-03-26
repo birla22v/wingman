b97f0a7e203ba4ff955024ffaaffa1aef6cdbd49
Vedika Birla <birla22v@mtholyoke.edu>
2015-03-26 11:18:17 -0400

    Taking decoder out

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,25 +13,25 @@ class User < ActiveRecord::Base
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates :username, :presence => true,:uniqueness => {:case_sensitive => false}
   attr_accessor :login
-  attr_accessor :image_data,:image
-  before_save :decode_image_data
-
-  def decode_image_data
-
-      if self.image_data.present?
-          # If image_data is present, it means that we were sent an image over
-          # JSON and it needs to be decoded.  After decoding, the image is processed
-          # normally via Paperclip.
-          if self.image_data.present?
-              data = StringIO.new(Base64.decode64(self.image_data))
-              data.class.class_eval {attr_accessor :original_filename, :content_type}
-              data.original_filename = self.id.to_s + ".png"
-              data.content_type = "image/png"
-
-              self.avatar = data
-          end
-      end
-    end
+  # attr_accessor :image_data,:image
+  # before_save :decode_image_data
+  #
+  # def decode_image_data
+  #
+  #     if self.image_data.present?
+  #         # If image_data is present, it means that we were sent an image over
+  #         # JSON and it needs to be decoded.  After decoding, the image is processed
+  #         # normally via Paperclip.
+  #         if self.image_data.present?
+  #             data = StringIO.new(Base64.decode64(self.image_data))
+  #             data.class.class_eval {attr_accessor :original_filename, :content_type}
+  #             data.original_filename = self.id.to_s + ".png"
+  #             data.content_type = "image/png"
+  #
+  #             self.avatar = data
+  #         end
+  #     end
+  #   end
 
    def ensure_authentication_token
      if authentication_token.blank?
